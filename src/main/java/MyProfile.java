db7d3bc878ccafd6bade653f97ad91d3d0c71e38
Sasha <Sasha@Sashas-Mac-mini.local>
2018-04-05 17:31:34 +0300

    added sign up page

---
+++ src/main/java/MyProfile.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.WebDriver;
+
+public class MyProfile {
+    private WebDriver driver;
+
+    public MyProfile(WebDriver driver) {
+        this.driver = driver;
+    }
+}
--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -1,9 +1,45 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SignUpPage {
-    WebDriver driver;
+    private WebDriver driver;
 
     public SignUpPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    private By registrationUsername = By.xpath("//input[@name='title']");
+    private By registrationMail = By.xpath("//input[@name='login']");
+    private By registrationPass = By.xpath("//input[@name='password']");
+    private By registrationButton = By.xpath("//button[@class='btn-link-i'][contains(text(),'Зарегистрироваться')]");
+
+    public SignUpPage typeUsername(String userName) {
+        driver.findElement(registrationUsername).sendKeys(userName);
+        return this;
+    }
+
+    public SignUpPage typeUserMail(String userEmail) {
+        driver.findElement(registrationMail).sendKeys(userEmail);
+        return this;
+    }
+
+    public SignUpPage typeUserPass(String userPass) {
+        driver.findElement(registrationPass).sendKeys(userPass);
+        return this;
+    }
+
+    public MyProfile clickRegistrationButton() {
+        driver.findElement(registrationButton).click();
+        return new MyProfile(driver);
+    }
+
+    public MyProfile signUp(String userName, String userEmail, String userPass) {
+        this.typeUsername(userName);
+        this.typeUserMail(userEmail);
+        this.typeUserPass(userPass);
+        this.clickRegistrationButton();
+        return new MyProfile(driver);
+    }
+
+
 }
