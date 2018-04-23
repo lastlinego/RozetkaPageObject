e4ce56cd64fc9a111b6627947561e1c8056eb08e
Sasha <Sasha@Sashas-Mac-mini.local>
2018-04-23 17:10:59 +0300

    added product to the cart

--- src/main/java/SearchResults.java
+++ src/main/java/SearchResults.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchResults {
    private WebDriver driver;
@@ -12,9 +15,17 @@ public class SearchResults {
     @FindBy(xpath = "//span[@class='sprite-side btn-link btn-link-green detail-buy-btn-link']/button[@type='submit']")
     private WebElement buyButton;
 
+    @FindBy(xpath = "//a[@name='goods-link'][contains(text(),'Встраиваемая посудомоечная машина BOSCH SMV 24 AX ')]")
+    private WebElement nameOfTheLot;
+
     public SearchResults clickBuyButton() {
         buyButton.click();
         return this;
+    }
+
+    public String getNameOfTheLot() throws InterruptedException {
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@name='goods-link'][contains(text(),'Встраиваемая посудомоечная машина BOSCH SMV 24 AX ')]")));
+        return nameOfTheLot.getText();
     }
 
 
