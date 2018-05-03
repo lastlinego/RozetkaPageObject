dcf715209ba8f7bef27fed7de61eef4d5e4b594e
Sasha <Sasha@Sashas-Mac-mini.local>
2018-05-03 12:57:33 +0300

    brands done

--- src/main/java/NotebooksPage.java
+++ src/main/java/NotebooksPage.java
@@ -1,9 +1,20 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class NotebooksPage {
     private WebDriver driver;
 
     public NotebooksPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    @FindBy(xpath = "//div[@class='pab-cell pab-img-45']")
+    private List<WebElement> brandsList;
+
+    public int numberOfTheBrands() {
+        return brandsList.size();
     }
 }
--- src/test/java/NotebookPageElements.java
+++ src/test/java/NotebookPageElements.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NotebookPageElements extends TestBase {
@@ -9,5 +10,7 @@ public class NotebookPageElements extends TestBase {
     public void checkTheBrandsNumber() {
         mainPage = PageFactory.initElements(driver, MainPage.class);
         mainPage.clickNotebooksBtn();
+        notebooksPage = PageFactory.initElements(driver, NotebooksPage.class);
+        Assert.assertEquals(notebooksPage.numberOfTheBrands(), 8);
     }
 }
